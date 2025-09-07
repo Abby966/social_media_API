58a4099a14e5374365cd499949ff813b8e5b3ff5
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-09-08 00:30:42 +0300

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Follow, Like, Comment, Bookmark
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("id","user","post","created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username","post__id")
+
 
 # Register your models here.
